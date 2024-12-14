d1b5650d8ce4bcef71890130ff7cf0122eadb776
최지철 <jife98@naver.com>
2024-12-14 22:44:48 +0900

    fix: 로그인 시, 신규 유저 및 기존유저에 따른 데이터 요청 구조 변경 및 화면전환 코드 수정

--- 24th-App-Team-1-iOS/Feature/LoginFeature/Sources/Presentation/Reactors/SignInViewReactor.swift
+++ 24th-App-Team-1-iOS/Feature/LoginFeature/Sources/Presentation/Reactors/SignInViewReactor.swift
@@ -19,13 +19,14 @@ import KeychainSwift
 
 public final class SignInViewReactor: Reactor {
     
-    private let createNewMemberUseCase: CreateNewMemberUseCaseProtocol
+    private let signInUseCase: SignInUserUseCaseProtocol
     private let globalService: WSGlobalServiceProtocol = WSGlobalStateService.shared
+    private var disposeBag = DisposeBag()
     public var initialState: State
     
     public struct State {
-        var accountRequest: CreateAccountRequest
-        @Pulse var signUpTokenResponse: CreateSignUpTokenResponseEntity?
+        var loginRequest: SignInUserRequest
+        @Pulse var signUpToken: SignUpTokenEntity?
         @Pulse var isLoading: Bool
         @Pulse var isShow: Bool
     }
@@ -36,19 +37,25 @@ public final class SignInViewReactor: Reactor {
     }
     
     public enum Mutation {
-        case setSignUpTokenResponse(CreateSignUpTokenResponseEntity)
+        case setSignUpToken(SignUpTokenEntity)
+        case setLoginUser(LoginUserEntity)
         case setLoading(Bool)
         case setTransitionFlag(Bool)
     }
     
-    public init(createNewMemberUseCase: CreateNewMemberUseCaseProtocol) {
-        self.createNewMemberUseCase = createNewMemberUseCase
+    public init(signInUseCase: SignInUserUseCaseProtocol) {
+        self.signInUseCase = signInUseCase
         self.initialState = State(
-            accountRequest: CreateAccountRequest(),
+            loginRequest: SignInUserRequest(socialType: "",
+                                              authorizationCode: "",
+                                              identityToken: "",
+                                              fcmToken: ""),
             isLoading: false,
             isShow: false
         )
     }
+    
+    //MARK: - Reactor Method
     
     public func mutate(action: Action) -> Observable<Mutation> {
         switch action {
@@ -64,22 +71,87 @@ public final class SignInViewReactor: Reactor {
                 return .empty()
             }
             UserDefaultsManager.shared.socialType = SocialTypes.kakao.rawValue
-            return executeSignUp(socialType: SocialTypes.apple.rawValue.uppercased(), authorizationCode: authorizationCode, identityToken: identityToken)
+            return executeTryLogin(socialType: SocialTypes.apple.rawValue.uppercased(), authorizationCode: authorizationCode, identityToken: identityToken)
         case .signInWithKakao:
             UserDefaultsManager.shared.socialType = SocialTypes.kakao.rawValue
             return handleKakaoLogin()
-                .flatMap { self.executeSignUp(socialType: SocialTypes.kakao.rawValue.uppercased(), authorizationCode: "", identityToken: $0) }
+                .flatMap { self.executeTryLogin(socialType: SocialTypes.kakao.rawValue.uppercased(), authorizationCode: "", identityToken: $0) }
             
         }
     }
     
-    func handleKakaoLogin() -> Observable<String> {
+    public func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setSignUpToken(let signUpTokenResponse):
+            newState.signUpToken = signUpTokenResponse
+            let expiredDate = Date.now.addingTimeInterval(30 * 60)
+                .toFormatLocaleString(with: .dashYyyyMMddhhmmss)
+                .toLocalDate(with: .dashYyyyMMddhhmmss)
+            UserDefaultsManager.shared.expiredDate = expiredDate
+        case .setLoading(let isLoading):
+            newState.isLoading = isLoading
+        case let .setTransitionFlag(isShow):
+            newState.isShow = isShow
+        case .setLoginUser(let userInfo):
+            KeychainManager.shared.set(value: userInfo.accessToken,
+                                       type: .accessToken)
+            KeychainManager.shared.set(value: userInfo.refreshToken,
+                                       type: .refreshToken)
+            
+            NotificationCenter.default.post(name: .showVoteMainViewController, object: nil)
+        }
+        return newState
+    }
+}
+
+    //MARK: - Mutation Logic
+
+extension SignInViewReactor {
+    private func executeTryLogin(socialType: String,
+                               authorizationCode: String,
+                               identityToken: String) -> Observable<Mutation> {
+        let fcmToken = UserDefaultsManager.shared.fcmToken ?? ""
+        let body = SignInUserRequest(socialType: socialType,
+                                            authorizationCode: authorizationCode,
+                                            identityToken: identityToken,
+                                            fcmToken: fcmToken)
+        
+        
+        return signInUseCase
+            .execute(body: body)
+            .asObservable()
+            .compactMap { $0 }
+            .flatMap { response -> Observable<Mutation> in
+                switch response {
+                case .success202(let signUpTokenEntity):
+                    return .concat(
+                        .just(.setLoading(false)),
+                        .just(.setSignUpToken(signUpTokenEntity)),
+                        .just(.setLoading(true))
+                    )
+                case .success200(let loginUserEntity):
+                    return .concat(
+                        .just(.setLoading(false)),
+                        .just(.setLoginUser(loginUserEntity)),
+                        .just(.setLoading(true))
+                    )
+                default:
+                    // 다른 경우 처리 (필요하다면 에러 처리 또는 빈 시퀀스 반환)
+                    return .just(.setLoading(false))
+                }
+            }
+
+    }
+    
+    private func handleKakaoLogin() -> Observable<String> {
         return Observable.create { observer in
             // 카카오톡 앱에 접근 가능할 때
             if (UserApi.isKakaoTalkLoginAvailable()) {
                 UserApi.shared.loginWithKakaoTalk { oauthToken, error in
                     if let error = error {
-                        print(error)
+                        WSLogger.error(category: "SignInViewReactor 카카오톡(설치O) 로그인 에러" ,
+                                       message: "\(error)")
                         observer.onError(error)
                     } else if let oauthToken = oauthToken {
                         observer.onNext(oauthToken.accessToken)
@@ -89,7 +161,8 @@ public final class SignInViewReactor: Reactor {
             } else { // 카카오톡 설치가 안되어 있으면
                 UserApi.shared.loginWithKakaoAccount { oauthToken, error in
                     if let error = error {
-                        print(error)
+                        WSLogger.error(category: "SignInViewReactor 카카오톡(설치X) 로그인 에러" ,
+                                       message: "\(error)")
                     } else if let oauthToken = oauthToken {
                         observer.onNext(oauthToken.accessToken)
                         observer.onCompleted()
@@ -98,49 +171,5 @@ public final class SignInViewReactor: Reactor {
             }
             return Disposables.create()
         }
-    }
-    
-    private func executeSignUp(socialType: String, authorizationCode: String, identityToken: String) -> Observable<Mutation> {
-        let fcmToken = UserDefaultsManager.shared.fcmToken ?? ""
-        let body = CreateSignUpTokenRequest(socialType: socialType,
-                                            authorizationCode: authorizationCode,
-                                            identityToken: identityToken,
-                                            fcmToken: fcmToken)
-        
-        let accessToken = KeychainManager.shared.get(type: .accessToken)
-        
-        if accessToken == nil {
-            return createNewMemberUseCase
-                .execute(body: body)
-                .asObservable()
-                .compactMap { $0 }
-                .flatMap { response -> Observable<Mutation> in
-                    return .concat(
-                        .just(.setLoading(false)),
-                        .just(.setSignUpTokenResponse(response)),
-                        .just(.setLoading(true))
-                    )
-                }
-        } else {
-            return .just(.setTransitionFlag(true))
-        }
-    }
-    
-    public func reduce(state: State, mutation: Mutation) -> State {
-        var newState = state
-        switch mutation {
-        case .setSignUpTokenResponse(let signUpTokenResponse):
-            newState.signUpTokenResponse = signUpTokenResponse
-            newState.accountRequest.signUpToken = signUpTokenResponse.signUpToken
-            let expiredDate = Date.now.addingTimeInterval(30 * 60)
-                .toFormatLocaleString(with: .dashYyyyMMddhhmmss)
-                .toLocalDate(with: .dashYyyyMMddhhmmss)
-            UserDefaultsManager.shared.expiredDate = expiredDate
-        case .setLoading(let isLoading):
-            newState.isLoading = isLoading
-        case let .setTransitionFlag(isShow):
-            newState.isShow = isShow
-        }
-        return newState
     }
 }
